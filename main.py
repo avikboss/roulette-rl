917416c1628f6880ceea09953694aa74dc16f10d
Avik <35409118+avikboss@users.noreply.github.com>
2021-04-29 12:19:23 -0500

    added monte carlo

--- main.py
+++ main.py
@@ -73,6 +73,49 @@ def qlearning(episodes=2000, alpha=0.01, gamma=0.9, eps=0.1):
     policy = {s:max(q[s], key=q[s].get) for s in q.keys()}
     return q, policy, rmse_list, policy_list
 
+def montecarlo_es(episodes, eps):
+#     Initialize, for all s ∈ S, a ∈ A(s):
+        # Q(s, a) ← arbitrary
+        # π(s) ← arbitrary
+        # Returns(s, a) ← empty list
+#     Repeat forever:
+        # Choose S0 ∈ S and A0 ∈ A(S0) s.t. all pairs have probability > 0
+        # Generate an episode starting from S0, A0, following π
+        # For each pair s, a appearing in the episode:
+        # G ← return following the first occurrence of s, a
+        # Append G to Returns(s, a)
+        # Q(s, a) ← average(Returns(s, a))
+        # For each s in the episode:
+        # π(s) ← argmaxa Q(s, a)
+
+    env = gym.make("Roulette-v0")
+    env.reset()
+
+    # initialize q, policy returns
+    q = {0:{a:0 for a in range(env.action_space.n)}}
+    policy = {0:0}
+    returns = {a:[] for a in range(env.action_space.n)}
+    rmse_list = []
+    policy_list = []
+
+    for _ in range(episodes):
+        a = env.action_space.sample() # exploring starts
+        taken_actions = []
+        done = False
+        while not done:
+            next_s, reward, done, _ = env.step(a) # simulate an episode, recording the first returns of each action
+            if a not in taken_actions:
+                taken_actions.append(a)
+                returns[a].append(reward)
+            a = get_eps_greedy_action(env,eps,policy)
+
+        q[0].update({a:np.mean(returns[a]) for a in taken_actions}) # at the end of an episode, update q and policy
+        policy[0] = max(q[0],key=q[0].get)
+        rmse_list.append(rmse(q,env))
+        policy_list.append(policy[0])
+
+    return q, policy, rmse_list, policy_list
+
 def get_eps_greedy_action(env,eps,policy):
     if np.random.random() < eps:
         return env.action_space.sample()
@@ -96,6 +139,7 @@ if __name__ == "__main__":
     # find the average error over many runs of the algorithms
     errors_ql = []
     errors_sr = []
+    errors_mc = []
     for i in range(EPOCHS):
         q, policy, rmse_ql, policy_list = qlearning(episodes=EPISODES,eps=0.01)
         errors_ql.append(rmse_ql)
@@ -103,12 +147,17 @@ if __name__ == "__main__":
         q, policy, rmse_sarsa, policy_list = sarsa(episodes=EPISODES,eps=0.01)
         errors_sr.append(rmse_sarsa)
 
+        q, policy, rmse_mc, policy_list = montecarlo_es(episodes=EPISODES,eps=0.01)
+        errors_mc.append(rmse_mc)
+
     mean_ep_error_ql = [np.mean([x[i] for x in errors_ql]) for i in range(EPISODES)]
     mean_ep_error_sr = [np.mean([x[i] for x in errors_sr]) for i in range(EPISODES)]
+    mean_ep_error_mc = [np.mean([x[i] for x in errors_mc]) for i in range(EPISODES)]
 
     plt.plot(mean_ep_error_ql)
     plt.plot(mean_ep_error_sr)
-    plt.legend(["QL","SR"])
+    plt.plot(mean_ep_error_mc)
+    plt.legend(["QL","SR","MC"])
     plt.show()
 
     # # q, policy, rmse_sarsa, policy_list = sarsa(episodes=EPISODES,eps=0.01)
